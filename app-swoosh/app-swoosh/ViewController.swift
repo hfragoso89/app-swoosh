71533d5d3913a942dead11a492121b6b102e0e91
Fragoso <hfragoso@MXN1800100-2.local>
2020-01-15 20:32:21 -0600

    main screen design

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Fragoso, Hector on 1/15/20.
+//  Copyright © 2020 Fragoso, Hector. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,48 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Fragoso, Hector on 1/15/20.
+//  Copyright © 2020 Fragoso, Hector. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
